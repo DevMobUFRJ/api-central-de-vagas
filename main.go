b4142445bf21c1d69f6d6ea575977a76b75b9eb4
Guilherme Franco <guilherme.franco@pigpeg.com>
2019-08-18 15:08:40 -0300

    .

--- main.go
+++ main.go
@@ -3,16 +3,10 @@ package main
 import (
 	"api-central-de-vagas/delivery/routes"
 	"api-central-de-vagas/resources/injection"
-	"io/ioutil"
-	"os"
 )
 
 func init() {
-	credentials := []byte(os.Getenv("FIREBASE_CREDENTIALS_JSON"))
-	err := ioutil.WriteFile("./firebase-adminsdk.json", credentials, 0644)
-	if err != nil {
-		panic(err)
-	}
+
 
 	injection.Inject()
 }
