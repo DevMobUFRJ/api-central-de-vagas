be0aaff431ce8ac39fece459df2f0900d4593d26
Guilherme Franco <guilherme.franco@pigpeg.com>
2019-08-18 22:42:43 -0300

    test

--- resources/database/mongodb.go
+++ resources/database/mongodb.go
@@ -1,11 +1,9 @@
 package database
 
 import (
-	"crypto/tls"
 	"fmt"
 	"github.com/globalsign/mgo"
 	"github.com/sirupsen/logrus"
-	"net"
 )
 
 func MongoDBConnect(url string) *mgo.Session {
@@ -14,26 +12,12 @@ func MongoDBConnect(url string) *mgo.Session {
 		panic("Can`t start because mongo uri is empty")
 	}
 
-	dialInfo, err := mgo.ParseURL(url)
-
-	if err != nil {
-		panic(err)
-	}
-
-	tlsConfig := tls.Config{}
-
-	dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
-		conn, err := tls.Dial("tcp", addr.String(), &tlsConfig)
-		return conn, err
-	}
-
-	panic(url)
-	session, err := mgo.DialWithInfo(dialInfo)
+	session, err := mgo.Dial(url)
 
 	if err != nil {
 		panic(err)
 	} else {
-		logrus.Info(fmt.Sprintf("Connected with MongoDB in database:[%s]", dialInfo.Database))
+		logrus.Info(fmt.Sprintf("Connected with MongoDB"))
 	}
 
 	session.SetMode(mgo.Monotonic, true)
